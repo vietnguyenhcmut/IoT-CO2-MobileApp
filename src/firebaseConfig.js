8a06458f1e91b4575ca53cd6bc5cb34f3fd66e85
viet01042001 <viet.nguyen.hcmut@gmail.com>
2023-01-02 10:59:02 +0700

    ok

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,10 +1,6 @@
 import { initializeApp } from "firebase/app";
 import {getDatabase} from "firebase/database";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCejL2VuPZk6C5eg2-yn9bUm7PyuxElO0s",
   authDomain: "iot-co2-729a7.firebaseapp.com",
